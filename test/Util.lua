2b015a55e04bd10c5fdaaea7ad9195295e305d0b
Kai Arnold <cdn_kai@yahoo.com>
2012-05-09 20:34:09 -0700

    adding file loading

--- test/Util.lua
+++ test/Util.lua
@@ -1,3 +1,11 @@
 function rand(min, max) 
 	return math.floor(math.random() * (max - min + 1) + min) 
 end
+function readLines(filename)
+	local retval = {}
+	local file = assert(io.open(filename, "r"))
+	for line in file:lines() do
+		retval[line] = 1
+	end
+	return retval
+end
