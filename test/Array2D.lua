47fd25aae0ba9609d6f6ff7612b6705707b29fa6
Kai Arnold <cdn_kai@yahoo.com>
2012-05-09 20:03:16 -0700

    made a working WRBoard:hasWord

--- test/Array2D.lua
+++ test/Array2D.lua
@@ -1,6 +1,6 @@
 Array2D = {}
 function Array2D:new(rows, cols)
-	local object = { rows = rows, cols = cols, items = {} }
+	local object = { rows = rows, cols = cols, size = rows*cols, items = {} }
 	setmetatable(object, self)
 	self.__index = self
 	return object
@@ -8,12 +8,26 @@ end
 function Array2D:get(r, c)
 	return self.items[r * self.cols + c]
 end
+function Array2D:getByIndex(i)
+	return self.items[i]
+end
 function Array2D:put(r, c, val)
 	self.items[r * self.cols + c] = val
 end
+function Array2D:putByIndex(i, val)
+	self.items[i] = val
+end
+function Array2D:getRowColFromIndex(i)
+	return math.floor(i / self.cols), i % self.cols
+end
+function Array2D:getIndexFromRowCol(r, c)
+	return r * self.cols + c
+end
+function Array2D:validRowCol(r, c)
+	return r >= 0 and r < self.rows and c >= 0 and c < self.cols
+end
 function Array2D:foreach(fn)
-	local size = self.rows * self.cols
-	for i = 0, size-1 do
+	for i = 0, self.size-1 do
 		fn(math.floor(i / self.cols), i % self.cols, self.items[i])
 	end
 end
--- test/WRBoard.lua
+++ test/WRBoard.lua
@@ -7,10 +7,48 @@ function WRBoard:new(rows, cols)
 	setmetatable(object, self)
 	self.__index = self
 	local function setRandom(row, col, val)
-		object.board:put(row, col, string.char(string.byte("A") + rand(0, 26)))
+		object.board:put(row, col, string.char(string.byte("A") + rand(0, 25)))
 	end
 	object.board:foreach(setRandom)
 	return object
+end
+
+-- these offsets make searching around our prospective letter easier to code
+WRBoard.wordSearchOffsets = { { r=0, c=1 }, { r=1, c=1 }, { r=1, c=0 }, { r=1, c=-1}, { r=0, c=-1 }, { r=-1, c=-1 }, { r=-1, c=0 }, { r=-1, c=1} }
+function WRBoard:hasWordImpl(word, wordIndex, foundLetterIndexes, startIndex)
+	local char = string.sub(word, wordIndex, wordIndex)
+	--print(wordIndex, char, startIndex, self.board:getByIndex(startIndex))
+	if self.board:getByIndex(startIndex) == char then
+		foundLetterIndexes:putByIndex(startIndex, wordIndex)
+		if wordIndex == #word then 
+			print("found it")
+			print(foundLetterIndexes)
+			return true -- we've found the entire word
+		else
+			-- try all of the adjacent letters to see if we can find anything
+			local r, c = self.board:getRowColFromIndex(startIndex)
+			for i,offset in pairs(WRBoard.wordSearchOffsets) do
+				local newR, newC = r + offset.r, c + offset.c
+				if self.board:validRowCol(newR, newC) and not foundLetterIndexes:get(newR, newC) then
+					if self:hasWordImpl(word, wordIndex + 1, foundLetterIndexes, self.board:getIndexFromRowCol(newR, newC)) then
+						return true
+					end
+				end
+			end
+		end
+		-- we didn't find a complete match; scrub out the found letter
+		foundLetterIndexes:putByIndex(startIndex, nil)
+	end
+	return false
+end
+function WRBoard:hasWord(word)
+	local foundLetterIndexes = Array2D:new(self.board.rows, self.board.cols)
+	for i = 0, self.board.size-1 do
+		if (self:hasWordImpl(word, 1, foundLetterIndexes, i)) then
+			return true
+		end
+	end
+	return false
 end
 function WRBoard:__tostring()
 	return self.board:__tostring()
