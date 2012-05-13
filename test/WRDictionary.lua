d42d631125cd4f5cbd5471e21dc22b991b7d22e7
Kai Arnold <cdn_kai@yahoo.com>
2012-05-13 15:44:08 -0700

    non-recursive version seems to work better

--- test/WRDictionary.lua
+++ test/WRDictionary.lua
@@ -33,7 +33,8 @@ function Trie:add(word)
 	local node = self.top
 	local index = 1
 	while index <= #word do
-		local letter = word:sub(index, index)
+		--local letter = word:sub(index, index)
+		local letter = word:byte(index)
 		if node[letter] == nil then
 			node[letter] = {}
 		end
@@ -42,19 +43,39 @@ function Trie:add(word)
 	end
 	node.word = true
 end
+tcalls = 0
 function Trie:hasNodeRecursiveImpl(node, word, index)
-	local letter = word:sub(index, index)
-	if (node[letter]) then
-		if (index < #word) then
+tcalls = tcalls + 1
+	--local letter = word:sub(index, index)
+	local letter = word:byte(index)
+	if node[letter] then
+		if index < #word then
 			return self:hasNodeRecursiveImpl(node[letter], word, index + 1)
 		end
 		return true, node[letter].word ~= nil
 	end
 	return false
 end
+function Trie:hasNodeImpl(word)
+	local node = self.top
+	for index = 1, #word do
+		--local letter = word:sub(index, index)
+		local letter = word:byte(index)
+		if node[letter] then
+			node = node[letter]
+		else
+			break
+		end
+		if index == #word then
+			return true, node.word ~= nil
+		end
+	end
+	return false
+end
 -- returns two bools, first true if the word fragment exists, second true if it's also a complete word
 function Trie:has(word)
-	return self:hasNodeRecursiveImpl(self.top, word, 1)
+	--return self:hasNodeRecursiveImpl(self.top, word, 1)
+	return self:hasNodeImpl(word)
 end
 
 d = readFileLinesToArray("words.lst")
@@ -62,6 +83,7 @@ t = Trie:new(d)
 --t = Trie:new({ "latexes" })
 
 function perfTest()
+	tcalls = 0
 	local pre = os.clock()
 	for i=1, 1000000 do
 		local w = d[rand(1, #d)]
@@ -70,5 +92,5 @@ function perfTest()
 			return
 		end
 	end
-	print("done in " .. (os.clock() - pre));
+	print("done in " .. (os.clock() - pre) .. " over " .. tcalls);
 end
