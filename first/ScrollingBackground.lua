dd0c26ffd6c26ec337c8193f5a17c41bd3f6b5c5
Kai Arnold <cdn_kai@yahoo.com>
2014-03-05 15:40:04 -0500

    scrolling works both directions

--- first/ScrollingBackground.lua
+++ first/ScrollingBackground.lua
@@ -12,12 +12,14 @@ function ScrollingBackground:new(image)
 	self.__index = self
 
 	object.left.anchorX = 0 -- anchor left
+	object.left.anchorY = 1 -- anchor from the bottom
 	object.left.x = 0
-	object.left.y = Aliases.dHeight / 2
+	object.left.y = Aliases.dHeight
 
 	object.right.anchorX = 0
+	object.right.anchorY = 1
 	object.right.x = Aliases.dWidth
-	object.right.y = Aliases.dHeight / 2
+	object.right.y = Aliases.dHeight
 	return object
 end
 
@@ -31,12 +33,23 @@ function ScrollingBackground:onFrame(elapsed, tPrevious)
 	self.left.x = self.left.x + xOffset
 	self.right.x = self.right.x + xOffset
 
-	function wrap(obj, objOther)
-		if (obj.x + obj.contentWidth) < 0 then
-			obj.x = objOther.x + objOther.contentWidth
-		print("wrapped")
+	if self.speed > 0 then
+		function wrapRight(obj, objOther)
+			if (obj.x + obj.contentWidth) < 0 then
+				obj.x = objOther.x + objOther.contentWidth
+				print("wrapRight")
+			end
 		end
+		wrapRight(self.left, self.right)
+		wrapRight(self.right, self.left)
+	elseif self.speed < 0 then
+		function wrapLeft(obj, objOther)
+			if (obj.x > Aliases.dWidth) then
+				obj.x = objOther.x - obj.contentWidth
+				print("wrapLeft")
+			end
+		end
+		wrapLeft(self.left, self.right)
+		wrapLeft(self.right, self.left)
 	end
-	wrap(self.left, self.right)
-	wrap(self.right, self.left)
 end
