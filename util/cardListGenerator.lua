41767e5a07ff2dbadd7c8d4e76be108c01e84bae
Kai Arnold <cdn_kai@yahoo.com>
2014-04-08 15:20:36 -0700

    cards.json generator working

--- util/cardListGenerator.lua
+++ util/cardListGenerator.lua
@@ -52,33 +52,71 @@ function findChildTagByProperty(children, propertyName, propertyValue)
 end
 function cardXMLToJSON(xml)
 
-	local cardNameNode = findChildTagByProperty(xml.child, "name", "CardName")
-	local cardName = findChildTagByName(cardNameNode.child, "enUS").value 
-	local cost = findChildTagWithProperty(xml.child, "name", "Cost").properties["value"]
-	local cardType = findChildTagWithProperty(xml.child, "name", "CardType").properties["value"]
-
-	local function getOptionalTagValue(children, propertyName, propertyValue, subPropertyValue)  
-		local tag = findChildTagWithProperty(children, propertyName, propertyValue)
+	local function getOptionalTagValue(children, propertyValue)  
+		local tag = findChildTagByProperty(children, "name", propertyValue)
 		if tag then
-			return tag.properties[subPropertyValue]
+			return tag.properties.value
 		end
 		return nil
 	end
-
-	local cardClass = getOptionalTagValue(xml.child, "name", "Class", "value")
-	local cardText = nil
-	local cardTextInHandNode = findChildTagByProperty(xml.child, "name", "CardTextInHand")
-	if cardTextInHandNode then
-		cardText = findChildTagByName(cardTextInHandNode.child, "enUS").value
+	local function getCardText(children, parentName)
+		local parent = findChildTagByProperty(children, "name", parentName)
+		if parent then
+			return findChildTagByName(parent.child, "enUS").value
+		end
 	end
 
+	local cardName = getCardText(xml.child, "CardName")
+	local cost = getOptionalTagValue(xml.child, "Cost")
+
+	if not cost then return nil end
+
+	local cardType = findChildTagByProperty(xml.child, "name", "CardType").properties["value"]
+	local cardClass = getOptionalTagValue(xml.child, "Class")
+	local cardText = getCardText(xml.child, "CardTextInHand")
+
+	local cardAtk = getOptionalTagValue(xml.child, "Atk")
+	local cardHealth = getOptionalTagValue(xml.child, "Health")
+	local durability = getOptionalTagValue(xml.child, "Durability")
+
+	local battlecry = getOptionalTagValue(xml.child, "Battlecry")
+	local taunt = getOptionalTagValue(xml.child, "Taunt")
+	local stealth = getOptionalTagValue(xml.child, "Stealth")
+	local freeze = getOptionalTagValue(xml.child, "Freeze")
+	local charge = getOptionalTagValue(xml.child, "Charge")
+	local divineShield = getOptionalTagValue(xml.child, "Divine Shield")
+	local windFury = getOptionalTagValue(xml.child, "Windfury")
+	local combo = getOptionalTagValue(xml.child, "Combo")
+	local overload = getOptionalTagValue(xml.child, "Recall")
+	local targetingText = getCardText(xml.child, "TargetingArrowText")
+
+	--------------------------
+	local function addOptionalTrait(trait, value)
+		if value then
+			local quoteEscape = string.gsub(value, '"', "'")
+			print('\t\t\t"' .. trait .. '": "' .. quoteEscape .. '",')
+		end
+	end
 	print('\t\t{')
 	print('\t\t\t"name": "' .. cardName .. '",')
-	print('\t\t\t"type": "' .. cardType .. '",')
-	if cardText then
-		print('\t\t\t"text": "' .. cardText .. '",')
-	end
 	print('\t\t\t"cost": "' .. cost ..'",')
+	print('\t\t\t"type": "' .. cardType .. '",')
+	addOptionalTrait("class", cardClass)
+	addOptionalTrait("atk", cardAtk)
+	addOptionalTrait("health", cardHealth)
+	addOptionalTrait("durability", durability)
+	addOptionalTrait("battlecry", battlecry)
+	addOptionalTrait("taunt", taunt)
+	addOptionalTrait("stealth", stealth)
+	addOptionalTrait("freeze", freeze)
+	addOptionalTrait("charge", charge)
+	addOptionalTrait("divineShield", divineShield)
+	addOptionalTrait("windFury", windFury)
+	addOptionalTrait("combo", combo)
+	addOptionalTrait("overload", overload)
+
+	addOptionalTrait("text", cardText)
+	addOptionalTrait("targetingText", targetingText)
 	print('\t\t\t"asset": "",')
 	print('\t\t},')
 
@@ -87,28 +125,30 @@ function oneCardTest(file)
 	local xml = newXMLParser()
 	local root = xml:loadFile(file)
 
-	print('{')
-	print('\t"cards":')
-	print('\t[')
-	-- for file in lfs.dir(dir) do
-	-- 	if (string.match(file, '\.png$')) then
-			cardXMLToJSON(root)
-	-- 	end
-	-- end
-	print('\t]')
-	print('}')
+	cardXMLToJSON(root)
 
 	return root
 end
 
-function generateHSCardsFromXML(file)
-	local xml = newXMLParser()
-	local root = xml:loadFile(file)
+function generateHSCardsFromXML(dir)
+	print('{')
+	print('\t"cards":')
+	print('\t[')
+	for file in lfs.dir(dir) do
+		if (string.match(file, '\.txt$')) then
+			local xml = newXMLParser()
+			local root = xml:loadFile(dir .. "/" .. file)
 
+			if root then
+				cardXMLToJSON(root)
+			end
+		end
+	end
+	print('\t]')
+	print('}')
 end
 
-xml = oneCardTest("c:/source/unity/HST/extractedstuff/cardxml/CS1_025.txt")
-xml = oneCardTest("c:/source/unity/HST/extractedstuff/cardxml/CS1_042.txt")
--- xml = generateHSCardsFromXML("c:/source/unity/HST/extractedstuff/cardxml")
-
-
+-- xml = oneCardTest("c:/source/unity/HST/extractedstuff/cardxml/ex1_133.txt")
+-- xml = oneCardTest("c:/source/unity/HST/extractedstuff/cardxml/new1_010.txt")
+-- xml = oneCardTest("c:/source/unity/HST/extractedstuff/cardxml/ex1_248.txt")
+generateHSCardsFromXML("c:/source/unity/HST/extractedstuff/cardxml")
